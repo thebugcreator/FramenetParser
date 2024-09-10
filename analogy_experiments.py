056bb757a906c497254516fc076954311c7449c7
thebugcreator <91310922+thebugcreator@users.noreply.github.com>
2024-09-10 10:12:52 +0200

    Stabilise the json generation stuff

--- analogy_experiments.py
+++ analogy_experiments.py
@@ -1,7 +1,7 @@
 import polars as pl
 import json
 
-def preprocess_test_set(df_path:str, output_path="experiments/inference/"):
+def preprocess_test_set(df_path:str, output_path="experiments/inference/", write_files=True) -> str:
     df = pl.read_json(df_path)
     # Get unique sentences
     df_sentence = df.unique(subset="sentence_id")
@@ -9,12 +9,29 @@ def preprocess_test_set(df_path:str, output_path="experiments/inference/"):
     df_sentence = df_sentence.with_columns(pl.Series([i for i in range(df_sentence.shape[0])]).alias("parser_id"))
     # Get the python strings ready
     sentences = []
-    for item in df_sentence.iter_rows(named=True):
-        instance = {}
-        instance["sentence"] = item["text"]
+    for item in df_sentence.select("text").to_series():
+        instance = {"sentence":item}
         sentences.append(json.dumps(instance))
-    with open(f"{output_path}test.json", "w") as file:
-        file.write("\n".join(sentences))
-    return "\n".join(sentences)
+    if write_files:
+        with open(f"{output_path}test.json", "w") as file:
+            file.write("\n".join(sentences))
+        df_sentence.write_json(output_path + "test_reference.json")
+    return sentences, df_sentence
 
-output = preprocess_test_set("data/test.json")
+# def process_eval_result(result_path:str="output_test.json", reference_path:str="experiments/inference/test_reference.json") -> pl.DataFrame:
+result_path:str="output_test.json"
+reference_path:str="experiments/inference/test_reference.json"
+df_reference = pl.read_json(reference_path)
+json_lines = []
+
+with open(result_path) as file:
+    for line in file:
+        json_line = json.loads(line)
+        json_lines.append(json_line)
+for record in json_lines:
+    tokens = record["words"]
+    frames = record["frames"]
+    frame_elements = record["frame_elements"]
+    frelements = list(zip(frames, frame_elements))
+
+
